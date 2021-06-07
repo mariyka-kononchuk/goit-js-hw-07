3dd13ba9e7566a924f0f98a6ff583b24640e46ea
Maria Kononchuk <mariyka.kononchuk@gmail.com>
2021-06-07 16:28:35 +0300

    Сделала 6-ю задачу

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,22 +1,18 @@
 const refs = {
-    input:document.querySelector('input#name-input'),
-    output:document.querySelector('span#name-output'),
+    input:document.querySelector('input#validation-input'),
 }
 
 refs.input.addEventListener('blur', onInputBlur); //потеря фокуса
-refs.input.addEventListener('focus', onInputBlur); //определение фокуса
+//refs.input.addEventListener('focus', onInputBlur); //определение фокуса
 
-const refs = {
-    input:document.querySelector('input#name-input'),
-    output:document.querySelector('span#name-output'),
-}
-
-refs.input.addEventListener('input', onInputText);
-
-function onInputText(event) {
+function onInputBlur(event) {
+    refs.input.textContent  = event.currentTarget.value;
     
-    refs.output.textContent = event.currentTarget.value;
-    if (refs.output.textContent.length === 0) {
-        refs.output.textContent ='незнакомец';
-    } 
+    if (refs.input.textContent.length === Number(refs.input.getAttribute('data-length'))) {
+        refs.input.classList.remove('invalid')
+        refs.input.classList.add('valid');
+    }
+    else
+    { refs.input.classList.add('invalid') };
+    
 }
